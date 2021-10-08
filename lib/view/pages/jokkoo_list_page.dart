5e15b08251c52b32bfb4f1a4ccb6d7858a7a3cc1
이지원 <jwlee@ijiwon-ui-MacBookPro.local>
2021-10-08 23:43:46 +0900

    2.2 족구장 리스트 페이지 구현

--- lib/view/pages/jokkoo_list_page.dart
+++ lib/view/pages/jokkoo_list_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_jokkoo/controller/jokkoo_controller.dart';
+import 'package:flutter_jokkoo/model/jokkoo_rez.dart';
+import 'package:flutter_jokkoo/model/jokkoojang.dart';
+import 'package:flutter_jokkoo/view/components/jokkoojang_item.dart';
+import 'package:flutter_jokkoo/view/pages/jokkoo_rez_page.dart';
+import 'package:get/get.dart';
 
 class JokkooListPage extends StatefulWidget {
   JokkooListPage({Key? key}) : super(key: key);
@@ -8,13 +14,34 @@ class JokkooListPage extends StatefulWidget {
 }
 
 class _JokkooListPageState extends State<JokkooListPage> {
+  JokkooController _controller = Get.find<JokkooController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("족구장 목록"),
       ),
-      body: Text("ListPage"),
+      body: Obx(
+        () => ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: _controller.jokkoojangList.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.jokkoojang.value =
+                    _controller.jokkoojangList[index];
+                _controller.stAbsTime.value = null;
+                _controller.endAbsTime.value = null;
+                Get.to(() => JokkooRezPage(DateTime.now()));
+              },
+              child: JokkoojangItem(
+                jokkoojang: _controller.jokkoojangList[index],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
